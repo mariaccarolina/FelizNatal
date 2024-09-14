c8af160febd3530ef188c72a860114bfbbd5922c
Maria Carolina <demycar24@gmail.com>
2024-09-14 19:13:43 -0300

    :recycle: refactor: refatorando o projeto

--- src/components/Inicio.vue
+++ src/components/Inicio.vue
@@ -1,28 +1,9 @@
 <script></script>
 
 <template>
-  <header>
-    <img src="../assets/logo (1).png" alt="logo marca" />
-    <nav>
-      <ul>
-        <li>
-          <img
-            src="https://github.com/mariaccarolina/StudioGhibli-/blob/main/src/assets/link-facebook.png?raw=true"
-            alt="logo facebook"
-          />
-        </li>
-        <li>
-          <img src="https://github.com/mariaccarolina/StudioGhibli-/blob/main/src/assets/link-instagram.png?raw=true" alt="instagram" />
-        </li>
-        <li>
-          <img src="https://github.com/mariaccarolina/StudioGhibli-/blob/main/src/assets/link-google.png?raw=true" alt="logo google" />
-        </li>
-        <li>
-          <img src="https://github.com/mariaccarolina/StudioGhibli-/blob/main/src/assets/link-twitter.png?raw=true" alt="logo twitter" />
-        </li>
-      </ul>
-    </nav>
-  </header>
+ 
 </template>
 
+<style lang="scss">
 
+</style>
