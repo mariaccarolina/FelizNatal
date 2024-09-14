4e7a29c3d88a782b6dc7c0cfe2a7031add50401c
Maria Carolina <demycar24@gmail.com>
2024-09-14 18:10:46 -0300

    :lipstick: feat: Instalando o sass e iniciando o estilo header

--- src/components/Inicio.vue
+++ src/components/Inicio.vue
@@ -1,17 +1,28 @@
 <script></script>
 
 <template>
-    <header>
-        <img src="../assets/logo (1).png" alt="logo marca">
-        <nav>
-           <ul>
-            <li>
-                <img src="" alt="">
-            </li>
-            <li></li>
-            <li></li>
-            <li></li>
-           </ul>
-        </nav>
-    </header>
+  <header>
+    <img src="../assets/logo (1).png" alt="logo marca" />
+    <nav>
+      <ul>
+        <li>
+          <img
+            src="https://github.com/mariaccarolina/StudioGhibli-/blob/main/src/assets/link-facebook.png?raw=true"
+            alt="logo facebook"
+          />
+        </li>
+        <li>
+          <img src="https://github.com/mariaccarolina/StudioGhibli-/blob/main/src/assets/link-instagram.png?raw=true" alt="instagram" />
+        </li>
+        <li>
+          <img src="https://github.com/mariaccarolina/StudioGhibli-/blob/main/src/assets/link-google.png?raw=true" alt="logo google" />
+        </li>
+        <li>
+          <img src="https://github.com/mariaccarolina/StudioGhibli-/blob/main/src/assets/link-twitter.png?raw=true" alt="logo twitter" />
+        </li>
+      </ul>
+    </nav>
+  </header>
 </template>
+
+
