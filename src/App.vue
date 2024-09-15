67f6b9da88fbf8714f9cd00a9734f4c24cb12473
Maria Carolina <demycar24@gmail.com>
2024-09-15 20:10:16 -0300

    :sparkles: feat: Adicionando o Footer

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import Inicio from "./components/Inicio.vue";
 import Cronometro from "./components/Cronometro.vue";
 import Doacao from "./components/Doacao.vue";
+import Rodape from "./components/Rodape.vue"
 </script>
 
 <template>
 <Inicio/>
 <Cronometro/>
 <Doacao/>
+<Rodape/>
 </template>
 
 <style>
---
+++ src/components/Rodape.vue
@@ -0,0 +1,32 @@
+<script></script>
+
+<template>
+    <footer>
+        <img class="logo" src="https://github.com/mariaccarolina/FelizNatal/blob/main/src/assets/logo%20(2).png?raw=true" alt="logomarca">
+        <nav>
+            <img src="https://github.com/mariaccarolina/FelizNatal/blob/main/src/assets/Frame%202.png?raw=true" alt="redes sociais">
+        </nav>
+    </footer>
+</template>
+
+<style lang="scss">
+footer{
+    display: flex;
+    justify-content: space-between;
+    padding: 0 10%;
+    background-color: #FFE9E8;
+
+    @media (max-width: 900px){
+        flex-direction: column;
+        align-items: center;
+        height: 15vh;
+        justify-content: space-around;
+
+        .logo{
+            width: 30vw;
+        }
+    }
+
+
+}
+</style>
