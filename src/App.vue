e8292e91a24dd8b7312799c0775ee11de80d415b
Maria Carolina <demycar24@gmail.com>
2024-09-15 00:11:31 -0300

    :sparkles: feat: Cronometro

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
-import Inicio from "./components/Inicio.vue"
+import Inicio from "./components/Inicio.vue";
+import Cronometro from "./components/Cronometro.vue"
 </script>
 
 <template>
 <Inicio/>
+<Cronometro/>
 </template>
 
 <style>
---
+++ src/components/Cronometro.vue
@@ -0,0 +1,56 @@
+<script>
+</script>
+
+<template>
+    <div class="container-cronometro">
+        <h3>Tempo limitado</h3>
+        <p>Nessas festas de fim de ano mande um presente para a pessoa amada e compartilhe a alegria do Natal.</p>
+        <h2>10d -24h - 01m  - 52s</h2>
+        <img src="https://github.com/mariaccarolina/FelizNatal/blob/main/src/assets/illustration.png?raw=true" alt="presentes de natal">
+    </div>
+</template>
+
+<style lang="scss">
+$font-titulos: "Archivo", sans-serif;
+$font-texto:"Roboto", sans-serif;
+
+.container-cronometro{
+    height: 60vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+
+    img{
+        width: 25vw;
+         @media (max-width: 450px){
+         width: 70vw;
+    }
+    }
+
+    h2{
+        color: #CD3C32;
+        font-weight: 600;
+        font-size: 32px;
+    }
+}
+
+h3{
+     font-family:  $font-titulos;
+        font-weight: 600;
+        font-size: 30px;
+
+         @media (max-width: 450px){
+         font-size: 24px;
+    }
+}
+
+p{
+    font-family: $font-titulos;
+    width: 30vw;
+    text-align: center;
+    @media (max-width: 450px){
+         width: 70vw;
+    }
+}
+</style>
